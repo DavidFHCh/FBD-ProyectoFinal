9ed75ef8d2119dacb4c108fe807dbba8601463f1
Davif <Davif@starKiller>
2016-12-21 14:07:57 -0600

    CAMBIANDOLE EL PEDO

--- src/javafxapplication2/FXMLDocumentController.java
+++ src/javafxapplication2/FXMLDocumentController.java
@@ -335,38 +335,20 @@ public class FXMLDocumentController implements Initializable {
       * @param tableview La tabla a llenar.
       */
     private void llenaTableView(ResultSet rs,TableView tableview){
-       ObservableList<ObservableList> data = FXCollections.observableArrayList();
           try{
             for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
-                //We are using non property style for making dynamic table
-                final int j = i;                
-                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
-                col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
-                    @Override
-                    public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
-                        return new SimpleStringProperty(param.getValue().get(j).toString());                        
-                    }                    
-                });
+            //We are using non property style for making dynamic table
+            final int j = i;                
+            TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
+            col.setCellValueFactory(new Callback<CellDataFeatures<ObservableList,String>,ObservableValue<String>>(){                    
+                public ObservableValue<String> call(CellDataFeatures<ObservableList, String> param) {                                                                                              
+                    return new SimpleStringProperty(param.getValue().get(j).toString());                        
+                }                    
+            });
 
-                tableview.getColumns().addAll(col); 
-            }
+            tableview.getColumns().addAll(col); 
+        }
 
-            /********************************
-             * Data added to ObservableList *
-             ********************************/
-            while(rs.next()){
-                //Iterate Row
-                ObservableList<String> row = FXCollections.observableArrayList();
-                for(int i=1 ; i<=rs.getMetaData().getColumnCount(); i++){
-                    //Iterate Column
-                    row.add(rs.getString(i));
-                }
-                data.add(row);
-
-            }
-
-            //FINALLY ADDED TO TableView
-            tableview.setItems(data);
           }catch(Exception e){
               e.printStackTrace();
               System.out.println("Error on Building Data");             
